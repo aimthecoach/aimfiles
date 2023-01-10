d68c16d6f95f438834af75dbbe4d7cb651eddc93
Ricardo Almeida <ricardoaimalmeida@gmail.com>
2023-01-10 20:46:14 +0000

    add docker installation script

---
+++ local/ubuntu-install-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo apt-get install nvidia-driver-515-server
+
+# Install dependencies
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+
+# Add Docker official GPG key
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+# Add the Docker repository to your system
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+# Create the docker group
+sudo groupadd docker
+
+# Add the current user to the docker group
+sudo usermod -aG docker $USER
+
+
